95d3b972516bfe8700c2b677929a07672c53c02a
Navneet Vishwakarma <navneetkumar.vishwakarma@serviceaide.com>
2018-07-16 12:15:31 +0530

    Initial Source Commit

---
+++ src/main/java/com/xask00/App.java
@@ -0,0 +1,13 @@
+package com.xask00;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
